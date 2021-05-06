1c7f895160a6aae316101689f8652f5ee467cb5e
jaakivask <jaak@adplus.ee>
2021-05-06 10:42:03 +0300

    GUI lisatud id ja osad muutujad kontrollerisse v2

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,17 +1,44 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class Controller {
 
     @FXML
-    public Button jsam;
     public Button paevahind;
+    public Button valikuhind;
+    public Button lõpeta;
+    public ComboBox riikValik;
+    public DatePicker lõpuKuupäev;
+    public DatePicker algusKuupäev;
+    public Button päringUuteAndmetega;
+    public Button SalvestaCsv;
+    public TableColumn tabelKuupäev;
+    public TableColumn tabelKõrgeimHind;
+    public TableColumn tabelMadalaimHind;
+    public TableColumn tabelKeskmineHind;
+    public TextField elektrihindMaxPeriood;
+    public TextField elektrihimdMinPeriood;
+    public TextField elektrihindKeskminePeriood;
     @FXML
     private Label lblOutput;
 
     public void sayHello() {
         lblOutput.setText("Hello FXML!");
+    }
+
+    public void päevahindKuva(ActionEvent actionEvent) {
+    }
+
+    public void valikuhindKuva(ActionEvent actionEvent) {
+    }
+
+    public void lõpetaProgramm(ActionEvent actionEvent) {
+    }
+
+    public void käivitaPäringUuteAndmetega(ActionEvent actionEvent) {
+    }
+
+    public void salvestaCSVFail(ActionEvent actionEvent) {
     }
 }
