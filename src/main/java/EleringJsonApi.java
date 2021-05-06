1f50ae0579f0b65e462a213a9e642d83f912c28e
zaiksb <peeter.niidas@ut.ee>
2021-05-06 23:19:47 +0300

    Lisatud json library'd Lisatud klassid Elektrihind, EleringJsonApi ja Päevahind

---
+++ src/main/java/EleringJsonApi.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class EleringJsonApi {
+    private String start;
+    // private String startTime;
+    private String end;
+    //private String endTime;
+    private String restEndPoint;
+    private String baseUrl="https://dashboard.elering.ee";
+    private URL url;
+
+    public EleringJsonApi(String restEndPoint) {
+        this.restEndPoint = restEndPoint;
+    }
+    //setter Elering API end point muutmiseks
+    public void setRestEndPoint(String restEndPoint) {
+        this.restEndPoint = restEndPoint;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    private void setEleringApiUrl(){
+
+        String urlString;
+        urlString =
+                this.baseUrl + this.restEndPoint + "?" + "start=" + URLEncoder.encode(this.start) + "&end=" + URLEncoder.encode(this.end);
+        System.out.println(urlString);
+        try{
+            this.url = new URL(urlString);
+        }catch (MalformedURLException e) {
+            System.err.println("URL oli vigane");
+            System.err.println("Väljastatud viga: " + e.getMessage());
+        }
+    }
+
+    public JSONObject getEleringData() throws IOException, ParseException {
+
+        setEleringApiUrl();
+
+        HttpURLConnection conn = (HttpURLConnection) this.url.openConnection();
+        //Elering API nõuab GET meetodit
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        //Testi kas ühendus on korras HTTP kood 200
+        int responsecode = conn.getResponseCode();
+
+        if (responsecode != 200) {
+            throw new RuntimeException("Http vastuskood vea korral: " + responsecode);
+        } else {
+            String inline = "";
+            Scanner scanner = new Scanner(url.openStream());
+
+            //Kirjuta kõik andmed JSON stringi
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+            }
+
+            //Sulge scanner
+            scanner.close();
+
+//            Using the JSON simple library parse the string into a json object
+            JSONParser parse = new JSONParser();
+            JSONObject data_obj = (JSONObject) parse.parse(inline);
+            return data_obj;
+        }
+    }
+}
---
+++ src/main/java/Päevahind.java
@@ -0,0 +1,68 @@
+import org.json.simple.JSONObject;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Päevahind {
+    private String riik;
+    private String perioodiAlgus;
+    private String perioodiLõpp;
+    private int minHind;
+    private int maxHind;
+    private int keskmineHind;
+    private List<Elektrihind> elektriHinnad;
+    private JSONObject data;
+    private String restEndPoint = "/api/nps/price";
+
+    public Päevahind(String riik, String perioodiAlgus, String perioodiLõpp, int minHind, int maxHind, int keskmineHind, List<Elektrihind> elektriHinnad, String restEndPoint) {
+        this.riik = riik;
+        this.perioodiAlgus = perioodiAlgus + " 00:00";
+        this.perioodiLõpp = perioodiLõpp + " 23:59";
+        this.minHind = minHind;
+        this.maxHind = maxHind;
+        this.keskmineHind = keskmineHind;
+        this.elektriHinnad = elektriHinnad;
+        this.data = data;
+        this.restEndPoint = restEndPoint;
+    }
+
+    public void setRiik(String riik) {
+        this.riik = riik;
+    }
+
+    public void setPerioodiAlgus(String perioodiAlgus) {
+        this.perioodiAlgus = perioodiAlgus;
+    }
+
+    public void setPerioodiLõpp(String perioodiLõpp) {
+        this.perioodiLõpp = perioodiLõpp;
+    }
+
+    public int getMinHind() {
+        return minHind;
+    }
+
+    public int getMaxHind() {
+        return maxHind;
+    }
+
+    public int getKeskmineHind() {
+        return keskmineHind;
+    }
+
+    public List<Elektrihind> getElektriHinnad() {
+        EleringJsonApi eleringJsonData = new EleringJsonApi(restEndPoint);
+        KuvaElektriHind elektriHind = new KuvaElektriHind();
+        eleringJsonData.setStart(perioodiAlgus);
+        eleringJsonData.setEnd(perioodiLõpp);
+        data = eleringJsonData.getEleringData();
+        elektriHind.määratudVahemikuMinMax((JSONObject) data.get("data"), riik);
+
+
+
+
+        return elektriHinnad;
+    }
+
+
+}
