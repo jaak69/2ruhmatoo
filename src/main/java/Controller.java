9023ffcd5a442c6e8f02c81da5d9a7a6f82a7fa3
jaakivask <jaak@adplus.ee>
2021-05-09 15:27:43 +0300

    json library lisatud build.gradle konfiguratsiooni

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,46 +1,84 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private String valitudRiik;
 
     @FXML
-    public Button paevahind;
-    public Button valikuhind;
+    public ToggleButton paevahind;
+    @FXML
+    public ToggleButton valikuhind;
+    @FXML
     public Button lõpeta;
+    @FXML
     public ComboBox riikValik;
+    @FXML
     public DatePicker lõpuKuupäev;
+    @FXML
     public DatePicker algusKuupäev;
+    @FXML
     public Button päringUuteAndmetega;
+    @FXML
     public Button SalvestaCsv;
+    @FXML
     public TableView tabelElektrihinnad;
+    @FXML
     public TableColumn tabelKuupäev;
+    @FXML
     public TableColumn tabelKõrgeimHind;
+    @FXML
     public TableColumn tabelMadalaimHind;
+    @FXML
     public TableColumn tabelKeskmineHind;
+    @FXML
     public TextField elektrihindMaxPeriood;
+    @FXML
     public TextField elektrihimdMinPeriood;
+    @FXML
     public TextField elektrihindKeskminePeriood;
 
     @FXML
     private Label lblOutput;
-
+    @FXML
     public void sayHello() {
         lblOutput.setText("Hello FXML!");
     }
-
+    @FXML
     public void päevahindKuva(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void valikuhindKuva(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void lõpetaProgramm(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void käivitaPäringUuteAndmetega(ActionEvent actionEvent) {
     }
-
+    @FXML
     public void salvestaCSVFail(ActionEvent actionEvent) {
+    }
+
+    public void showPäevahind(String valitudRiik){
+        if (valikuhind.isSelected()){
+            valikuhind.setSelected(false);
+        }
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        valitudRiik = (String) riikValik.getValue();
+
+        showPäevahind(valitudRiik);
+
     }
 }
