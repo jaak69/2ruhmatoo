b2fbff90a70986562bbcae546f73b62b9220cb41
jaakivask <jaak@adplus.ee>
2021-05-10 19:55:17 +0300

    Controller

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,18 +1,26 @@
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) throws IOException, ParseException {
         String algus = "2021-05-01";
-        String riigiTähis = "ee";
+        String riigiTähis = "fi";
 
         Paevahind paevahind = new Paevahind(riigiTähis);
         System.out.println("Tundide hinnad " + paevahind.getPäevaHinnad());
         System.out.println("Keskmine hind " + paevahind.keskmineHind());
         System.out.println("Max hind " + paevahind.maksimaalneHind());
         System.out.println("Min hind " + paevahind.minimaalneHind());
+
+
+        System.out.println(Month.from(LocalDate.now()).getDisplayName(TextStyle.FULL_STANDALONE,new Locale("et","EE")));
     }
 }
